03e81f11f2564d7d6ed43431a4e68d32dd40de36
Jaewook Oh <kyle.oh95@gmail.com>
2021-01-24 18:47:56 +0900

    2021-01-24 백준 2750

---
+++ baekjoon/2750.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	w := bufio.NewWriter(os.Stdout)
+	r := bufio.NewReader(os.Stdin)
+	defer w.Flush()
+	var N int
+	fmt.Fscanf(r, "%d\n", &N)
+	arr := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscanf(r, "%d\n", &arr[i])
+	}
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+	for _, v := range arr {
+		fmt.Fprintln(w, v)
+	}
+}
