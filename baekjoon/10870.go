ab32fe2e25c2fedb42125ad7698eaed67e0c270e
Jaewook Oh <kyle.oh95@gmail.com>
2021-01-27 22:07:41 +0900

    2021-01-27 백준 10870

---
+++ baekjoon/10870.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	w = bufio.NewWriter(os.Stdout)
+	r = bufio.NewReader(os.Stdin)
+)
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	defer w.Flush()
+	var N int
+	fmt.Fscanf(r, "%d\n", &N)
+	fmt.Fprintf(w, "%d\n", fib(N))
+}
