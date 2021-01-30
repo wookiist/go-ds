31fc36e0d5514a0caabfc6639962e7c0ef71e144
Jaewook Oh <kyle.oh95@gmail.com>
2021-01-30 20:40:43 +0900

    2021-01-30 백준 10808 - 2

--- baekjoon/10808.go
+++ baekjoon/10808.go
@@ -22,3 +22,31 @@ func main() {
 		fmt.Fprintf(w, "%d ", len(r.FindAllString(s, -1)))
 	}
 }
+
+/*
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	alpha := make([]int, 26)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	for _, v := range scanner.Bytes() {
+		alpha[v-'a']++
+	}
+
+	wr := bufio.NewWriter(os.Stdout)
+	defer wr.Flush()
+	for _, v := range alpha {
+		wr.WriteString(strconv.Itoa(v))
+		wr.WriteByte(' ')
+	}
+}
+*/
