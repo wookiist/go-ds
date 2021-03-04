89457db3a8605626f2a5cdecfd9b51a9fe53b460
Jaewook Oh <kyle.oh95@gmail.com>
2021-03-04 20:47:43 +0900

    2021-03-04 boj 2440

---
+++ baekjoon/2440.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	w = bufio.NewWriter(os.Stdout)
+	r = bufio.NewReader(os.Stdin)
+)
+
+func main() {
+	defer w.Flush()
+	var N int
+	fmt.Fscanf(r, "%d\n", &N)
+	for i := 0; i < N; i++ {
+		for j := N - i; j > 0; j-- {
+			fmt.Fprint(w, "*")
+		}
+		fmt.Fprintln(w)
+	}
+}
