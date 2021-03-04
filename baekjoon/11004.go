ee860a42b5fd69a2810ec94a80fc7279ab71d2b6
Jaewook Oh <kyle.oh95@gmail.com>
2021-03-04 20:12:34 +0900

    2021-03-04 boj 11004

---
+++ baekjoon/11004.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var (
+	w = bufio.NewWriter(os.Stdout)
+	r = bufio.NewReader(os.Stdin)
+)
+
+func main() {
+	defer w.Flush()
+	var N, K int
+	fmt.Fscanf(r, "%d %d\n", &N, &K)
+	arr := getInts()
+	sort.Ints(arr)
+	fmt.Fprintln(w, arr[K-1])
+}
+
+func getInts() []int {
+	s, _ := r.ReadString('\n')
+	s = strings.TrimSuffix(s, "\n")
+	t := strings.Fields(s)
+	res := make([]int, len(t))
+	for i := range t {
+		res[i], _ = strconv.Atoi(t[i])
+	}
+	return res
+}
